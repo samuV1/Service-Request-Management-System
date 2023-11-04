547a7b514054b787ff0d8dfef63492ce3a8d788c
samuV1 <samuel.emailprofissional@gmail.com>
2023-11-04 13:56:26 -0300

    Creating the Startup.cs class and changing the Program.cs class

--- Service_Request_Management_System/Program.cs
+++ Service_Request_Management_System/Program.cs
@@ -1,3 +1,24 @@
+using Service_Request_Management_System;
+
+namespace SistemaAtendimentoSuporte;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args)
+           .Build()
+           .Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
+
+/*
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -25,3 +46,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+*/
